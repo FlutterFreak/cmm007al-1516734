fc5c2f8d9b1151746e98641f45c0902f5628024a
a <a@a.com>
2016-05-04 12:45:40 +0100

    adminer

---
+++ blog.php
@@ -0,0 +1,81 @@
+
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+-->
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+    <script src="bootstrap/js/jquery-1.12.0.min.js" type="text/javascript"></script>
+    <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <link href="style.css" rel="stylesheet" type="text/css"/>
+    <title>BugTracker</title>
+</head>
+<body>
+
+<div class="container border">
+
+    <header>
+
+
+        <div class="row border">
+
+
+
+
+
+            <div class="col-md-12">
+
+                <h2>myBlog</h2>
+                <h3>because the internet needs to know what I think</h3>
+            </div>
+
+        </div>
+
+
+
+    </header>
+
+    <div class="row">
+        <div class="col-md-12 min-height">
+
+            <div class="navbar">
+                <div class="row">
+                    <div class="col-md-12"><h5><a href="blog.html">All Blog Items</a></h5></div>
+
+                </div>
+                <div class="row">
+                    <div class="col-md-12"><h5><a href="blog.html">work Items</a></h5></div>
+
+                </div>
+                <div class="row">
+                    <div class="col-md-12"><h5><a href="blog.html">University Items</a></h5></div>
+
+                </div>
+                <div class="row">
+                    <div class="col-md-12"><h5><a href="blog.html">Family items</a></h5></div>
+
+                </div>
+                <div class="row">
+                    <div class="col-md-12"><h5><a href="add.html">Insert a Blog Item</a></h5></div>
+
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html><?php
+/**
+ * Created by PhpStorm.
+ * User: 1516734
+ * Date: 04/05/2016
+ * Time: 12:45
+ */
