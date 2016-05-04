8e43b7d18a7af74bdca8af1325cbe2cd58a13624
a <a@a.com>
2016-05-04 10:44:50 +0100

    adminer

---
+++ blog.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `blogView` (
+  `blogitemID` int(10) NOT NULL AUTO_INCREMENT,
+  `entryTitle` varchar(50) NOT NULL,
+  `entrySummary` varchar(250) NOT NULL,
+  `Category` varchar(50) NOT NULL,
+`submitter` varchar(50) NOT NULL,
+  PRIMARY KEY (`blogitemID`))
