50f247dc916a927bdf23e976465a660c9f7ea67f
a <a@a.com>
2016-08-18 11:41:43 +0100

    adminer

---
+++ diary.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1516734
+ * Date: 18/08/2016
+ * Time: 11:04
+ */
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <title> My Diary </title>
+</head>
+<body>
+
+<div class="container">
+
+    <header>
+        <img src="image/blog.png" alt="logo" width="140px">
+        <div class="head">
+            <h1> myDiary</h1>
+            <h3> Keeping track of all my thoughts</h3>
+        </div>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="diary.php">All Diary Items</a></li>
+            <li><a href="diary.php">Work Diary Items</a></li>
+            <li><a href="diary.php">Unuversity Diary Items</a></li>
+            <li><a href="diary.php">Family Diary Items</a></li>
+            <li><a href="add.php">Insert Diary Items</a></li>
+
+        </ul>
+    </nav>
+
+    <article>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+            like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+            like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </article>
+
+    <footer> Designed by Nirdesh Kulhar 2016 </footer>
+
+</div>
+
+</body>
+</html>
